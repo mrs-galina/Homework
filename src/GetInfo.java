42b23ee34a1b0333bedd484b1c013034f7107262
Galya <143288791+mrs-galina@users.noreply.github.com>
2024-04-17 23:58:05 +0300

    Update GetInfo.java

--- src/GetInfo.java
+++ src/GetInfo.java
@@ -2,11 +2,11 @@ public final class GetInfo {
     private GetInfo() {}
     public static final String COMPANY_NAME = "Company";
 
-    public static void Description() {
+    public static void description() {
         System.out.println("This is the best company in the world");
     }
 
-    public static int CountSalary(int age, int years ){
+    public static int countSalary(int age, int years ){
         return age*years*10000;
     }
 }
